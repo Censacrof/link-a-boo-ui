ece2e0911f25ee3e4ec4268717af7fee79ae8cae
Francesco Galisi <galisifrancesco@gmail.com>
2024-04-30 01:36:56 +0200

    Add logo (draft)

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
+import Image from "next/image";
+import logo from "../public/linkaboo-logo.jpeg";
+
 export default function Home() {
   return (
     <div className="main-grid">
       <div className="main-grid-breakout flex h-dvh flex-col items-center justify-center gap-4 bg-gray-50 text-gray-950">
-        <h2 className="text-3xl">Link &apos;a boo!</h2>
-        <form className="flex flex-col gap-2">
+        <Image src={logo} alt="Link 'a boo" className="w-[min(25rem,80dvw)]" />
+        <div className="flex flex-col gap-2">
           <input
             placeholder="https://foo.bar.com/baz"
-            className="h-6 rounded-full border border-solid border-gray-950 px-2"
+            className="h-6 rounded border border-solid border-gray-950 px-2"
           />
-          <button className="h-8 rounded bg-gray-200 px-2">
+          <button className="h-8 rounded bg-gray-300 px-2 hover:bg-gray-200 active:bg-gray-100 ">
             Make it short!
           </button>
-        </form>
+        </div>
       </div>
-
-      <div className="main-grid-main">gigio</div>
     </div>
   );
 }
