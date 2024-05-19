e4ae80fbd2212f7bca537b225b78c34b1c212f9c
Francesco Galisi <galisifrancesco@gmail.com>
2024-05-19 23:53:07 +0200

    Add alias input and labels

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import Button from "./components/Button";
+import FormControl from "./components/FormControl/FormControl";
+import FormControlLabel from "./components/FormControl/FormControlLabel";
 import Input from "./components/Input";
 
-export default function Home() {
+export default function Page() {
   return (
     <div className="main-grid">
-      <div className="main-grid-breakout flex h-dvh flex-col items-center justify-center gap-4 text-gray-950">
+      <div className="main-grid-breakout flex h-dvh flex-col items-center justify-center gap-4">
         <div className="flex flex-col gap-2">
-          <Input placeholder="https://foo.bar.com/baz" />
+          <FormControl>
+            <FormControlLabel>Enter long link</FormControlLabel>
+            <Input placeholder="https://foo.bar.com/baz" />
+          </FormControl>
+          <FormControl isOptional>
+            <FormControlLabel>Add alias</FormControlLabel>
+            <Input placeholder="cool-page" />
+          </FormControl>
           <Button>Make it short!</Button>
         </div>
       </div>
