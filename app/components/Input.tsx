277d81bfc5b469cec4fbadd884d256055c8b1608
Francesco Galisi <galisifrancesco@gmail.com>
2024-05-19 23:51:50 +0200

    Use FormControl in Input

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -2,6 +2,7 @@
 
 import clsx from "clsx";
 import { InputHTMLAttributes, forwardRef } from "react";
+import { useFormControlContext } from "./FormControl/FormControl";
 
 export type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
@@ -9,8 +10,11 @@ export default forwardRef<HTMLInputElement, InputProps>(function Input(
   { className, ...props },
   ref,
 ) {
+  const formControl = useFormControlContext();
   return (
     <input
+      id={formControl?.id}
+      required={!formControl?.isOptional}
       className={clsx(
         "h-8 rounded border border-solid border-gray-950 px-2",
         className,
