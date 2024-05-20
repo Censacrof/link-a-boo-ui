24c672bd0de6cafc46b998ebf130116bd3da1346
Francesco Galisi <galisifrancesco@gmail.com>
2024-05-20 21:35:16 +0200

    Improve IconButton

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -5,13 +5,17 @@ import { ReactNode, forwardRef } from "react";
 import Button, { ButtonProps } from "./Button";
 
 export type IconButtonProps = Omit<ButtonProps, "children"> & {
-  icon: ReactNode;
+  icon?: ReactNode;
 };
 
 export default forwardRef<HTMLButtonElement, IconButtonProps>(
   function IconButton({ className, icon, ...props }, ref) {
     return (
-      <Button className={clsx("rounded-full", className)} {...props} ref={ref}>
+      <Button
+        className={clsx("aspect-square rounded-full", className)}
+        {...props}
+        ref={ref}
+      >
         {icon}
       </Button>
     );
